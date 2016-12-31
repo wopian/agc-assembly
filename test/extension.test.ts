a2f43f519442277b05f2fdc9db3f4a5636621440
James Harris <wopiancitizen123@gmail.com>
2016-12-31 15:53:11 +0000

    Add some unit tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,19 +4,92 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+//import * as assert from 'assert';
+import { expect, assert, chai } from 'chai';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
+import { workspace, Uri } from 'vscode';
+import * as extension from './../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite('Chai Tests', () => {
+    let foo = 'bar';
+    test('expect foo be a string', () => {
+        expect(foo).to.be.a('string');
+    });
+    test('expect foo to equal bar', () => {
+        expect(foo).to.equal('bar');
+    });
+    test('expect foo to have length 3', () => {
+        expect(foo).to.have.length(3);
+    });
+    test('expect foo to be a string length 3', () => {
+        expect(foo).to.be.a('string')
+            .with.length(3);
+    });
+});
 
-    // Defines a Mocha unit test
-    test("Something happened", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('Array', () => {
+    suite('#indexOf()', () => {
+        test('should return -1 when the value is not present', () => {
+            //[1, 2, 3].indexOf(5).should.equal(-1);
+            //[1, 2, 3].indexOf(0).should.equal(-1);
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+            assert.equal(-1, [1, 2, 3].indexOf(0));
+        });
+    });
+});
+
+suite('Assembly Spec', () => {
+    suite('AGC Assembly', () => {
+        test('should open .agc', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.agc')).then((document) => {
+            });
+        });
+
+        test('should detect the file as AGC', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.agc')).then((document) => {
+                expect(document.languageId).to.equal('agc');
+            });
+        });
+    });
+
+    suite('AGS Assembly', () => {
+        test('should open .aea', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.aea')).then((document) => {
+            });
+        });
+
+        test('should detect the file as AGS', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.aea')).then((document) => {
+                expect(document.languageId).to.equal('aea');
+            });
+        });
+    });
+
+    suite('Argus', () => {
+        test('should open .argus', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.argus')).then((document) => {
+            });
+        });
+
+        test('should detect the file as Argus', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.argus')).then((document) => {
+                expect(document.languageId).to.equal('argus');
+            });
+        });
+    });
+
+    suite('Binsource', () => {
+        test('should open .binsource', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.binsource')).then((document) => {
+            });
+        });
+
+        test('should detect the file as Binsource', () => {
+            workspace.openTextDocument(Uri.parse('./../spec/SPEC.binsource')).then((document) => {
+                expect(document.languageId).to.equal('binsource');
+            });
+        });
     });
 });
