ef248954b9deb3397515fe4a21e124879fce1afd
James Harris <wopiancitizen123@gmail.com>
2017-01-26 00:24:16 +0000

    Strip linting from extension

--- src/util/debug.ts
+++
@@ -1,9 +0,0 @@
-import { StatusBarAlignment, window } from 'vscode';
-
-export function debugStatusBar(DEBUG?: boolean) {
-    if (DEBUG) {
-        const statusBar = window.createStatusBarItem(StatusBarAlignment.Left, -100);
-        statusBar.text = 'AGC Assembly Debug';
-        statusBar.show();
-    }
-}
